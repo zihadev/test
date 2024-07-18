0ba3591eb9d38ec92af8df8eec6fb11bad074382
zihadev <34001897+zihadev@users.noreply.github.com>
2024-07-18 09:47:38 +0200

    freegui

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-print("To jest polecenie nr1")
-print("To jest polecenie nr2")
+import FreeSimpleGUI as fsg
 
+label1 = fsg.Text("Select file(s) to compress: ")
+input_box1 = fsg.InputText()
+add_button1 = fsg.FilesBrowse("Choose")
+
+label2 = fsg.Text("Select destination: ")
+input_box2 = fsg.InputText()
+add_button2 = fsg.FolderBrowse("Choose")
+compress_button = fsg.Button("Compress")
+
+layout = [
+    [label1],
+    [input_box1,add_button1],
+    [label2],
+    [input_box2, add_button2],
+    [compress_button]
+]
+
+# Tworzenie okna z tytułem "My App"
+window = fsg.Window('My App', layout)
+
+# Pętla wydarzeń
+window.read()
+window.close()
